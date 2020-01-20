9c9efdb42015b8d4b931aca741c647f457f32f08
Zach Brown <zachmb22@gmail.com>
2020-01-20 19:51:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,7 +30,17 @@ def using_insert(array, ele)
   array[4].insert = ele
 end
 
-def
-
+def using_uniq(array)
+  array.uniq
 end
 
+def using_flatten(array)
+ array.flatten
+end
+
+def using_delete(array,string)
+ array.delete(string)
+end
+
+def using_delete_at(array,integer)
+  array[integer].delete
